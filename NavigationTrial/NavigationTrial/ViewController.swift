b88041ce3b5a9f6361a4b55c00966ecd825910f7
J Reigh <58107794+j2reigh@users.noreply.github.com>
2020-05-08 19:16:50 +0900

    1차 세미나 과제1 2020-04-18

---
+++ NavigationTrial/NavigationTrial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationTrial
+//
+//  Created by 이지윤 on 2020/04/24.
+//  Copyright © 2020 이지윤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
