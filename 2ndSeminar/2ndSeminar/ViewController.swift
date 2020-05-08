65bc1f8415bffbb77e3d6fe5962c157b0fc5fc55
J Reigh <58107794+j2reigh@users.noreply.github.com>
2020-05-08 19:12:04 +0900

    2nd Seminar 2020-04-25

---
+++ 2ndSeminar/2ndSeminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2ndSeminar
+//
+//  Created by 이지윤 on 2020/04/25.
+//  Copyright © 2020 이지윤. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
