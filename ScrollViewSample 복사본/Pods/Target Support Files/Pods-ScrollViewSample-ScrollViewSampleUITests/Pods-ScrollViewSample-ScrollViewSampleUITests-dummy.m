47f70529e0721c9a3e1d48e670a9951c125167f6
j2reigh <58107794+j2reigh@users.noreply.github.com>
2020-06-15 21:21:56 +0900

    파일정리

--- ScrollViewSample 복사본/Pods/Target Support Files/Pods-ScrollViewSample-ScrollViewSampleUITests/Pods-ScrollViewSample-ScrollViewSampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScrollViewSample_ScrollViewSampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ScrollViewSample_ScrollViewSampleUITests
-@end
--- ScrollViewSample 복사본/Pods/Target Support Files/Pods-ScrollViewSample/Pods-ScrollViewSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ScrollViewSample : NSObject
-@end
-@implementation PodsDummy_Pods_ScrollViewSample
-@end
